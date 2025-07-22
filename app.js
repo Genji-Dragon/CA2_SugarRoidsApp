1d28e735be897f4b3bc8f47edfdc8a236396aea9
Genji-Dragon <ziyang219@gmail.com>
2025-07-22 20:29:48 +0800

    added fawaz code on app.js

--- app.js
+++ app.js
@@ -61,22 +61,6 @@ const checkAdmin = (req, res, next) => {
   res.redirect('/');
 };
 
-// Middleware for form validation
-const validateRegistration = (req, res, next) => {
-    const { username, email, password, address, contact, role } = req.body;
-
-    if (!username || !email || !password || !address || !contact || !role) {
-        return res.status(400).send('All fields are required.');
-    }
-    
-    if (password.length < 6) {
-        req.flash('error', 'Password should be at least 6 or more characters long');
-        req.flash('formData', req.body);
-        return res.redirect('/register');
-    }
-    next();
-};
-
 // Define routes
 app.get('/', checkAuth, (req, res) => {
   res.send(`<h1>Welcome, ${req.session.user.username} (${req.session.user.role})</h1><a href="/logout">Logout</a>`);
