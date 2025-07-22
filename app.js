4591d020159190d08caf1c7ce9402144e7632ab3
Genji-Dragon <ziyang219@gmail.com>
2025-07-22 16:34:28 +0800

    revert back to products to test the code

--- app.js
+++ app.js
@@ -259,41 +259,41 @@ app.post('/addProduct', upload.single('image'),  (req, res) => {
 });
 
 
-app.get('/updateProductCandy/:id',checkAuthenticated, checkAdmin, (req,res) => {
-    const candyId = req.params.id;
-    const sql = 'SELECT * FROM products WHERE candyId = ?';
+app.get('/updateProduct/:id',checkAuthenticated, checkAdmin, (req,res) => {
+    const productId = req.params.id;
+    const sql = 'SELECT * FROM products WHERE productId = ?';
 
     // Fetch data from MySQL based on the product ID
-    connection.query(sql , [candyId], (error, results) => {
+    connection.query(sql , [productId], (error, results) => {
         if (error) throw error;
 
         // Check if any product with the given ID was found
         if (results.length > 0) {
             // Render HTML page with the product data
-            res.render('updateCandy', { candy: results[0] });
+            res.render('updateProduct', { product: results[0] });
         } else {
             // If no product with the given ID was found, render a 404 page or handle it accordingly
-            res.status(404).send('Candy not found');
+            res.status(404).send('Product not found');
         }
     });
 });
 
-app.post('/updateProductCandy/:id', upload.single('image'), (req, res) => {
-    const candyId = req.params.id;
+app.post('/updateProduct/:id', upload.single('image'), (req, res) => {
+    const productId = req.params.id;
     // Extract product data from the request body
-    const { candyName, quantity, price } = req.body;
+    const { name, quantity, price } = req.body;
     let image  = req.body.currentImage; //retrieve current image filename
     if (req.file) { //if new image is uploaded
         image = req.file.filename; // set image to be new image filename
     } 
 
-    const sql = 'UPDATE products SET candyName = ? , quantity = ?, price = ?, image =? WHERE candyId = ?';
+    const sql = 'UPDATE products SET productName = ? , quantity = ?, price = ?, image =? WHERE productId = ?';
     // Insert the new product into the database
-    connection.query(sql, [candyName, quantity, price, image, candyId], (error, results) => {
+    connection.query(sql, [name, quantity, price, image, productId], (error, results) => {
         if (error) {
             // Handle any error that occurs during the database operation
-            console.error("Error updating candy:", error);
-            res.status(500).send('Error updating candy');
+            console.error("Error updating product:", error);
+            res.status(500).send('Error updating product');
         } else {
             // Send a success response
             res.redirect('/inventory');
